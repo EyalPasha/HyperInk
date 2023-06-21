97f52dbaea3e376cafad8edbf87cf74422443ec9
TaLevy5 <talm2011@gmail.com>
2023-06-21 15:35:58 +0300

    v_5.3

---
+++ client/js/navBars.js
@@ -0,0 +1,7 @@
+$(function () { //this function presents the navBar in each html page
+    $('#navbar').load('../HTML/navBar.html');
+  });
+
+  $(function () {  //this function presents the footer in each html page
+    $('#footer_nav').load('../HTML/footer.html');
+  });
---
+++ client/js/navBars_indexHTML.js
@@ -0,0 +1,7 @@
+$(function () { //this function presents the navBar in index.html file
+    $('#navbar').load('/client/HTML/navBar.html');
+  });
+
+  $(function () {  //this function presents the footer in index.html file
+    $('#footer_nav').load('/client/HTML/footer.html');
+  });
